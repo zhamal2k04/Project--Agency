5a46a4bd5a658464d268f32d14afcfa0926ec706
unknown <garippolosov1203@gmail.com>
2023-07-26 12:53:43 +0500

    added MainCard jsx and css file into compnents folder

---
+++ src/containers/BlogsPage/components/MainCard/MainCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./MainCard.css";
+
+const MainCard = () => {
+  return (
+    <div className="MainCard-container">
+      <div className="maincardLeft-container">
+        <div className="lowerContent-text">
+          <h1>The real deal</h1>
+          <p>
+            As strategic partners in our clients' businesses, we are ready to
+            take on any challenge as our own. Solving real problems require
+            empathy and collaboration, and we strive to bring a fresh
+            perspective to every opportunity. We make design and technology more
+            accessible and give you tools to measure success. <br />
+            <br />
+            <br /> We are visual storytellers in appealing and captivating ways.
+            By combining business and marketing strategies, we inspire audiences
+            to take action and drive real results.
+          </p>
+        </div>
+      </div>
+      <div className="maincardRight-container"></div>
+    </div>
+  );
+};
+
+export default MainCard;
--- src/containers/BlogsPage/container/BlogsPage.jsx
+++ src/containers/BlogsPage/container/BlogsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./BlogsPage.css"
+import MainCard from '../components/MainCard/MainCard'
 
 
 const BlogsPage = () => {
   return (
     <div className='blogsPage-container'>
-        <h1>BlogPage</h1>
+        <MainCard/>
     </div>
   )
 }
