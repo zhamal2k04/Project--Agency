3d4c73aefc71383a9a80df1c29a839489ea7eddf
unknown <garippolosov1203@gmail.com>
2023-07-29 14:54:08 +0500

    reworked with HomePage and AboutPage components

--- src/containers/AboutPage/components/NewsSection/NewsSection.jsx
+++ src/containers/AboutPage/components/NewsSection/NewsSection.jsx
@@ -10,7 +10,7 @@ const NewsSection = () => {
       <div className="sectionsHandler">
         <div className="sectionLeft">
           <img src={newsImg} alt="newsImg" />
-          <p>13 May, 2022</p>
+          <p>13 May, 2023</p>
           <h1>Teamwork as a team is the best way to</h1>
           <span>
             <p>
@@ -21,7 +21,7 @@ const NewsSection = () => {
         </div>
         <div className="sectionRight">
           <img src={newsImg2} alt="newsImg" />
-          <p>13 May, 2022</p>
+          <p>13 May, 2023</p>
           <h1>How to surviving on the great industry age</h1>
           <span>
             <p>
